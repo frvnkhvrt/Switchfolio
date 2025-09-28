97dda8821eca6408e91ac695067aae8836edcd0e
Francisco Hurtado <frvnkhvrt@gmail.com>
2025-09-27 23:07:34 -0500

    Minor update.

--- src/data/Common/data.ts
+++ src/data/Common/data.ts
@@ -122,7 +122,7 @@ const newsText =
   "Lock in, stay wired, and claim first dibs on the chaos I'm unleashing next."
 
   const supportText =
-  "Dig my vibe? Your support powers my work."
+  "Dig my vibe? Fuel my grind."
 
 export {
   projects,
